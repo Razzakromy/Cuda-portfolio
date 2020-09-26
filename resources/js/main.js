757c712e261a3899560eb2ed296ef7d3d9838aef
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2020-09-26 12:21:31 +0600

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
          var hash = this.hash;
           $('html, body').animate({
             scrollTop: $(hash).offset().top
-          }, 1000, function(){
+          }, 500, function(){
             window.location.hash = hash;
           });
         } 
