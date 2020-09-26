9dd723679fe2c11ede20ad4ae5f50c13ace664da
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2020-09-26 12:15:04 +0600

    added all files

---
+++ resources/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $(".js_services_section").waypoint(function (direction) {
+        if (direction == "down") {
+            $("nav").addClass("sticky")
+        } else {
+            $("nav").removeClass("sticky")
+        }
+    })
+    // MIX IT UP (PORTFOLOIO)
+    var mixer = mixitup(".container")
+
+    // SMOOOTH SCROLL FOR IE/EDGE/SAFARI
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+         var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1000, function(){
+            window.location.hash = hash;
+          });
+        } 
+      });
+    });
+
+//     $("a").on("click", function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault();
+
+//             const hash = this.hash
+//             $("html, body").animate({
+//                 scrollTop: $(hash).offset().top}, 800, function () {
+//                  window.location.hash = hash;   
+//                 })
+//         }
+//     })
+// });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
