c776617c91c6e09870e92d29f5ebcf5536ea8469
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2020-10-10 17:46:58 +0600

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,47 +1,37 @@
 $(document).ready(function () {
-    $(".js_services_section").waypoint(function (direction) {
-        if (direction == "down") {
-            $("nav").addClass("sticky")
-        } else {
-            $("nav").removeClass("sticky")
+  // Sticky Menu
+  $(".js_services_section").waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  });
+  // MIX IT UP (PORTFOLOIO)
+  var mixer = mixitup(".container");
+
+  // SMOOOTH SCROLL FOR IE/EDGE/SAFARI
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        300,
+        function () {
+          window.location.hash = hash;
         }
-    })
-    // MIX IT UP (PORTFOLOIO)
-    var mixer = mixitup(".container")
-
-    // SMOOOTH SCROLL FOR IE/EDGE/SAFARI
-
-    $("a").on('click', function(event) {
-
-        if (this.hash !== "") {
-          event.preventDefault();
-         var hash = this.hash;
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 300, function(){
-            window.location.hash = hash;
-          });
-        } 
-      });
-    });
-
-//     $("a").on("click", function (event) {
-//         if (this.hash !== "") {
-//             event.preventDefault();
-
-//             const hash = this.hash
-//             $("html, body").animate({
-//                 scrollTop: $(hash).offset().top}, 800, function () {
-//                  window.location.hash = hash;   
-//                 })
-//         }
-//     })
-// });
+      );
+    }
+  });
+});
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
